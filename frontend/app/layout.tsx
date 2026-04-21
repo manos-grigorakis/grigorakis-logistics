d7deb2575cfb08bd299188de18e135f20fb0a837
manos <manos.grigorakis03@gmail.com>
2026-04-21 16:50:59 +0300

    feat: add navigation bar to layout
    fix: HTML lang to el

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/layout/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="gr"
+      lang="el"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="flex flex-col min-h-full">
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
