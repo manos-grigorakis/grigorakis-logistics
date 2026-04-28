94cddeb54167bfd582fcc652aab0588b4fa68da4
Manos Grigorakis <manos.grigorakis03@gmail.com>
2026-04-28 16:54:37 +0300

    Remove Google Analytics from layout
    Removed Google Analytics conditional rendering from layout.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -39,11 +39,11 @@ export default function RootLayout({
         </LazyMotion>
 
         {/* Analytics */}
-        /* {process.env.NODE_ENV === "production" && (
+        {/* {process.env.NODE_ENV === "production" && (
           <GoogleAnalytics gaId="G-YL947VHQXY" />
         )}
         <Analytics />
-        <SpeedInsights /> */
+        <SpeedInsights /> */}
       </body>
     </html>
   );
